d7042762a635108d52cac280a88e825f19d350ae
idontknowmyuser <alecmac456@gmail.com>
2025-07-21 19:58:01 -0400

    attempt to start opmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Matthew_Danny.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Matthew_Danny.java
@@ -1,4 +1,17 @@
 package org.firstinspires.ftc.teamcode.opmodes;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.teamcode.drive.DriveConstants;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+@Autonomous
+public class Matthew_Danny extends LinearOpMode {
 
-public class Matthew_Danny {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        SampleMecanumDrive drivetrain = new SampleMecanumDrive(hardwareMap);
+        waitForStart();
+        frontLeft.setPower
+    }
 }
+
+
