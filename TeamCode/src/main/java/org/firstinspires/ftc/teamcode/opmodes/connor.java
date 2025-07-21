267999690273869caf1646649625638cc8743e6f
Alicia074 <aliciaesova@gmail.com>
2025-07-21 19:56:36 -0400

    Update connor.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/connor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/connor.java
@@ -1,4 +1,13 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
-public class connor {
+import com.qualcomm.hardware.digitalchickenlabs.OctoQuad;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.subsystems.Odometry;
+
+@TeleOp
+public class TestOdoPods extends LinearOpMode {
+    
 }
