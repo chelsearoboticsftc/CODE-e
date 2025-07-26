d55b07b194f36344a5d0afc936d2acdb65c54ce8
rhoffman2117 <rhoffman21@comcast.net>
2025-07-26 18:16:25 -0400

    Create robModeTest.java
    starting to learn opModes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/robModeTest.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name = "Rob's Test")
+public class robModeTest extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        waitForStart();
+
+    }
+}
