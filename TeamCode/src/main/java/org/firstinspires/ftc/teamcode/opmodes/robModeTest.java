8156fcbd200e5b7de2b2c87b41350c48bba389f9
rhoffman2117 <rhoffman21@comcast.net>
2025-07-26 22:42:30 -0400

    Running motor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/robModeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/robModeTest.java
@@ -4,11 +4,23 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+
 @TeleOp(name = "Rob's Test")
 public class robModeTest extends LinearOpMode {
+    private DcMotor motor1 = null;
+
     @Override
     public void runOpMode() throws InterruptedException {
+        motor1 = hardwareMap.get(DcMotor.class, "motorTest");
+        telemetry.addData("Status", "Initialize");
+        telemetry.update();
         waitForStart();
-
+        telemetry.addData("Status", "Started");
+        telemetry.update();
+        motor1.setPower(0.5);
+        sleep(1000);
+        motor1.setPower(0);
+        telemetry.addData("Status", "Stopped");
+        telemetry.update();
     }
 }
