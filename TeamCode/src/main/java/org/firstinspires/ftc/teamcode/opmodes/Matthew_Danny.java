c658de312d34e87a646d7eb37bda2207cffe8255
eidoo1gaN <vaughnwbrodie@gmail.com>
2025-07-28 19:58:32 -0400

    create running motor using joystick

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Matthew_Danny.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Matthew_Danny.java
@@ -1,4 +1,36 @@
 package org.firstinspires.ftc.teamcode.opmodes;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
-public class Matthew_Danny {
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+@Autonomous
+public class Matthew_Danny extends LinearOpMode{
+    @Override
+    public void runOpMode() throws InterruptedException {
+        DcMotorEx MotorTest = hardwareMap.get(DcMotorEx.class, "MotorTest");
+        DistanceSensor distanceSensor = hardwareMap.get(DistanceSensor.class, "distanceSensor");
+        waitForStart();
+        double speed;
+        while (opModeIsActive()) {
+
+            telemetry.addData("Gamepad Rightstick Y value",gamepad1.right_stick_y);
+            telemetry.update();
+
+            speed = -1 * gamepad1.right_stick_y;
+            MotorTest.setPower(speed);
+
+
+
+
+        }
+
+
+    }
 }
+
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/connor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/connor.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
 import com.qualcomm.hardware.digitalchickenlabs.OctoQuad;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.HardwareMap;
@@ -8,6 +9,11 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.teamcode.subsystems.Odometry;
 
 @TeleOp
-public class TestOdoPods extends LinearOpMode {
-    
+@Disabled
+
+public class connor extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
 }
